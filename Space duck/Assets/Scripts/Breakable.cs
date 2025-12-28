51209874e71ed4a460a88e661276643dceaf4d72
Zalihun69 <159771111+Zalihun69@users.noreply.github.com>
2025-12-28 21:16:16 +0100

    doboz robbantás
    Mostmár balklikkre robbannak szét

--- Space duck/Assets/Scripts/Breakable.cs
+++ Space duck/Assets/Scripts/Breakable.cs
@@ -1,51 +1,70 @@
 ﻿using UnityEngine;
-using System.Collections; 
+using System.Collections;
+using UnityEngine.InputSystem;
 
 public class Breakable : MonoBehaviour
 {
     [SerializeField] private GameObject _replacement;
     [SerializeField] private float _breakForce = 5;
     [SerializeField] private float _collisionMultiplier = 100;
-    [SerializeField] private bool _broken;
+    [SerializeField] private float _breakRange = 5f;
 
     [Header("Audio")]
     public AudioSource playerAudioSource;
     public AudioClip boxExplosionSound;
 
     [Header("Break Delay")]
-    public float visualBreakDelay = 1.0f; 
+    public float visualBreakDelay = 1.0f;
 
-    void OnCollisionEnter(Collision collision)
+    private bool _broken;
+
+    void Update()
     {
-        if (_broken) return;
-
-        if (collision.relativeVelocity.magnitude >= _breakForce)
+        if (Mouse.current.leftButton.wasPressedThisFrame && !_broken && Time.timeScale > 0)
         {
-            _broken = true;
+            Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+            RaycastHit[] hits = Physics.RaycastAll(ray, _breakRange);
 
-            if (playerAudioSource != null && boxExplosionSound != null)
+            foreach (RaycastHit hit in hits)
             {
-                playerAudioSource.PlayOneShot(boxExplosionSound);
+                if (hit.transform == transform)
+                {
+                    _broken = true;
+                    StartCoroutine(BreakSequence(hit.point));
+                    break;
+                }
             }
-
-            StartCoroutine(BreakSequence(collision));
         }
     }
-    private IEnumerator BreakSequence(Collision collision)
+
+    private IEnumerator BreakSequence(Vector3 impactPoint)
     {
+        if (playerAudioSource != null && boxExplosionSound != null)
+        {
+            playerAudioSource.PlayOneShot(boxExplosionSound);
+        }
+
         yield return new WaitForSeconds(visualBreakDelay);
 
-        var replacement = Instantiate(_replacement, transform.position, transform.rotation);
+        if (TryGetComponent<Renderer>(out var rend)) rend.enabled = false;
+        if (TryGetComponent<Collider>(out var coll)) coll.enabled = false;
+
+        // Szilánkok létrehozása
+        GameObject replacement = Instantiate(_replacement, transform.position, transform.rotation);
+
+        // --- ÚJ RÉSZ: A szilánkok (darabok) törlése 5 másodperc múlva ---
+        Destroy(replacement, 5f);
 
         var rbs = replacement.GetComponentsInChildren<Rigidbody>();
-
         foreach (var rb in rbs)
         {
-            rb.AddExplosionForce(collision.relativeVelocity.magnitude * _collisionMultiplier, collision.contacts[0].point, 2);
+            rb.AddExplosionForce(_breakForce * _collisionMultiplier, impactPoint, 2);
+
             Vector3 randomScatter = new Vector3(Random.Range(-1f, 1f), Random.Range(0.5f, 2f), Random.Range(-1f, 1f));
             rb.AddForce(randomScatter * _collisionMultiplier * 0.5f, ForceMode.Impulse);
         }
 
+        // Az eredeti (most már láthatatlan) doboz törlése
         Destroy(gameObject);
     }
 }
